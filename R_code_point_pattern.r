a0fe5b031f9a46e8ac7aec7fb021248da31ce401
EnzodeCa98 <115997735+EnzodeCa98@users.noreply.github.com>
2022-10-27 20:54:42 +0200

    Update R_code_point_pattern.r

--- R_code_point_pattern.r
+++ R_code_point_pattern.r
@@ -20,3 +20,11 @@ points(covid_planar, pch = 19)
 cl <- colorRampPalette(c('yellow','orange','red'))(100) # 
 plot(density_map, col = cl)
 points(covid_planar)
+
+#####Errors#############
+## Error in ppp(x = lon, y = lat, c(-180, 180), c(-90, 90)) : 
+  could not find function "ppp"
+## library(spatstat)
+Error: pacchetto ‘spatstat.data’ richiesto da ‘spatstat’ non è stato trovato
+In addition: Warning message:
+il pacchetto ‘spatstat’ è stato creato con R versione 4.2.1 
