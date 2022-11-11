8ddf10d28a45c115e0d6925a64dd745e54bf0289
EnzodeCa98 <115997735+EnzodeCa98@users.noreply.github.com>
2022-11-11 17:51:42 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -10,3 +10,27 @@ p224r63_2011 <- brick("p224r63_2011_masked.grd")
 plot(p224r63_2011)
 cl <- colorRampPalette(c("black","grey","light grey")) (100) 
 plot(p224r63_2011,col = cl)
+
+## solution 1 to select only one band  of light with the color we want to see
+plot(p224r63_2011$B1sre, col = cl) ## in order to select only the one band of color we want to see)
+
+## solution 2, if we know one element present in the data
+plot(p224r63_2011[[1]], col =cl)
+
+## exercise1: change colorRamppalette with colours from dark blue to light blue
+
+clv <- colorRampPalette(c("deepskyblue4", "deepskyblue2", "darkslategray2")) (100)
+plot(p224r63_2011,col = clv)
+plot(p224r63_2011[[1]], col =clv)
+
+par(mfrow = c(1,2)) ## multiframe 
+plot(p224r63_2011[[1]], col =clv) ##blue band
+plot(p224r63_2011[[2]], col =clv) ## green band
+
+## exercise2: put a multiframe with 4 bands, 2X2 images
+
+par(mfrow = c(2,2)) ## multiframe 
+plot(p224r63_2011[[1]], col =clv) ##blue band
+plot(p224r63_2011[[2]], col =clv) ## green band
+plot(p224r63_2011[[3]], col =clv) ## red band
+plot(p224r63_2011[[4]], col =clv) ## 
